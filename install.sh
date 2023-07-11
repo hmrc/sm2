77b3410bcc9b8ddb3026e7865d36a9563bfe90dd
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2023-07-11 13:54:11 +0100

    BDOG-2696 moves binary to /usr/local/bin

--- install.sh
+++ install.sh
@@ -12,7 +12,17 @@ if [[ "$(uname -p)" == "arm" ]]; then
     TARGET_ARCH="arm64"
 fi
 
+echo "Downloading sm2 v$TARGET_VERSION..."
+
 cd &&
 curl -s -L -O "https://github.com/hmrc/sm2/releases/download/v$TARGET_VERSION/sm2-$TARGET_VERSION-$TARGET_OS-$TARGET_ARCH.zip" &&
 unzip sm2-$TARGET_VERSION-$TARGET_OS-$TARGET_ARCH.zip && rm sm2-$TARGET_VERSION-$TARGET_OS-$TARGET_ARCH.zip &&
 chmod +x sm2
+
+echo "Moving sm2 to /usr/local/bin..."
+
+sudo mv ~/sm2 /usr/local/bin/sm2
+
+echo "Successfully installed"
+
+sm2 --version
